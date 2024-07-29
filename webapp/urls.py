47272788ad6acf69c2f4f03e1e101ab39e66268e
shiv-freelance <154948859+shiv-freelance@users.noreply.github.com>
2024-07-29 07:18:32 +0530

    29th July 2024

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from webapp.views import home_page
+from webapp.views import home_page, register
+
 
 urlpatterns = [
-    path('', view=home_page, name=""),
+    path("", view=home_page, name=""),
+    path("register", view=register, name="register"),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
 def home_page(request):
     # return HttpResponse('Hello World!!!')
     return render(request, 'webapp/index.html')
+
+
+# - Register
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # return redirect('webapp/login.html')
+    
+    context = {"form": form}
+
+    return render(request, 'webapp/register.html', context=context)
 
